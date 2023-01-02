d0f1df946791295f9c79a6fb74a295af5e3ea86c
oushneek <oushneek@gitlab.com>
2023-01-02 14:51:13 +0600

    Priject initiated with AdminLte3

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+@include('partials.head')
+
+<body class="hold-transition sidebar-mini layout-fixed">
+<div class="wrapper">
+    @include('partials.header')
+
+    <!-- Left side column. contains the logo and sidebar -->
+    @include('partials.sidebar')
+
+<!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        @yield('content')
+    </div>
+
+    @include('partials.footer')
+</div>
+
+@vite(['resources/js/app.js'])
+
+@stack('third_party_scripts')
+
+@stack('page_scripts')
+</body>
+</html>
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,8 @@
+<!-- Main Footer -->
+<footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+        <b>Version</b> 3.0.5
+    </div>
+    <strong>Copyright &copy; 2014-2022 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights
+    reserved.
+</footer>
