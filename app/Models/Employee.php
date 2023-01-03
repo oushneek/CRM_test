1753ce5d75620e2ec39d25e1d0f019c321496a5e
oushneek <oushneek@gitlab.com>
2023-01-03 13:02:19 +0600

    Employees CRUD initiated

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -5,14 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Company extends Model
+class Employee extends Model
 {
     use HasFactory;
 
     protected $guarded=['id'];
 
-    public function employees()
-    {
-        return $this->hasMany(Employee::class);
+    public function company(){
+        return $this->belongsTo(Company::class, 'company_id', 'id');
     }
+
+
 }
